d8e95d7f1c4b77bbd60542ffdb7cd4bdfcc5fed5
Will <avatec2312@gmail.com>
2022-11-22 23:42:11 -0500

    Adding turn buttons

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ const gameBoard = (()=>{
 
 })()
 
-
-
+const turn = 0;
 
 const PlayerFactory = (type)=>{
 
+    // Player should check if it's his turn
+
+    // Player should choose a cell and put an "X" or "O"
+
+    // Player 
 
     return {type}
 }
