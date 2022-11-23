dd4c867e2bffb546d5249fb297b5203d9abb4ef3
Will <avatec2312@gmail.com>
2022-11-23 03:06:57 -0500

    Creating pickTurn listener

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 const gameBoard = (()=>{
-    const gameBoard = [];
 
+    // Maybe be a Type error here down
+    const gameBoard = [
+        ["","",""]
+        ["","",""]
+        ["","",""]
+];
+    // Maybe a Type error up here
+})
+
+let choice;
+
+const pickTurn = (()=>{
+
+    // Buttons Container reference
+
+    const buttons = document.querySelectorAll('.buttons button');
+
+    // When button is clicked the selection should be stored in a variable.
+    // This value should be used for later when the game start.
+
+    buttons.forEach(button=>{
+        button.addEventListener("click",()=>{
+                console.log({button});
+                choice = button.textContent;
+                console.log({choice})
+        }
+    ,false)
+    })
 })()
 
 const turn = 0;
