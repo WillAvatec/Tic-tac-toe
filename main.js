1e8cbc9485e097a8529b2a46cb9b27d59bedcac4
Will <avatec2312@gmail.com>
2022-11-24 03:06:23 -0500

    Check turn

--- main.js
+++ main.js
@@ -1,66 +1,92 @@
 const gameBoard = (()=>{
 
-    const pattern = [
-        "","","",
-        "","","",
-        "","",""
-    ];
-
     const cols = document.querySelectorAll('.col');
 
-    // --- Renderize the pattern 
+    let pattern = [];
+    let choice;
 
-    /* Se debe hacer lo siguiente:
-    
-        1) Iterar por el patron
-        2) Se guarda el valor del valor iterado en ese momento
-        3) Escribe ese valor en la columna 
-
-    */
-
-    const renderBoardFromArray = (function(){
+/*     const renderPattern = (function(){
 
         for (let i = 0; i < pattern.length; i++) {
             const element = pattern[i];
             cols[i].textContent = element;
         }
+    })() */
+
+
+/* 
+    const pickSymbol = (()=>{
+
+        // Buttons Container reference
+
+        const buttons = document.querySelectorAll('button');
+
+        // When button is clicked the selection should be stored in a variable.
+        // This value should be used for later when the game start.
+
+        buttons.forEach(button=>{
+            button.addEventListener("click",(e)=>{
+                    choice = button.textContent;
+                    gameFlow()
+            }
+        ,false)
+        })
+    })()
+ */
+
+
+    const gameFlow  = (()=>{
+
+        const PlayerFactory = (type,symbol,turn)=>{
+            return {type,symbol,turn}
+        }
+    
+        const playerX = PlayerFactory("player","X",true)
+        const playerO = PlayerFactory("player","O",false)
+        let myTurn = playerX;
+
+        // Check which player turn is 
+
+        function checkTurn(myTurn,target){
+            if(myTurn == playerX){
+                myTurn = playerO;
+                renderSymbol(target,playerX);
+
+            }
+            else if(myTurn == playerO){
+                myTurn = playerX;
+                renderSymbol(target,playerO);
+            }
+        }
+
+        // When clicking on col add that value to the pattern and render again.
+
+        function renderSymbol(target,player){
+            if (player.turn){
+                target.textContent = player.symbol;
+            }
+        }
+
+        // DOM Listener
+        
+        cols.forEach((col)=>{
+            col.addEventListener("click",()=> {
+                if(!col.textContent=="") return console.log("you already clicked in this cell");
+                checkTurn(myTurn, col);
+            });
+        })
+        
+
+
+        // Game should check if someone's win
+
+
+        // If he has win then render 'span' than declares winner 
+
+
+
     })()
 
 })()
 
-let choice;
-
-const pickTurn = (()=>{
-
-    // Buttons Container reference
-
-    const buttons = document.querySelectorAll('.buttons button');
-
-    // When button is clicked the selection should be stored in a variable.
-    // This value should be used for later when the game start.
-
-    buttons.forEach(button=>{
-        button.addEventListener("click",()=>{
-                console.log({button});
-                choice = button.textContent;
-                console.log({choice});
-        }
-    ,false)
-    })
-})()
-
-const turn = 0;
-
-const PlayerFactory = (type)=>{
-
-    // Player should check if it's his turn
-
-    // Player should choose a cell and put an "X" or "O"
-
-    // Player should check if he's win
-
-    // If he has win then render 'span' than declares winner 
-
-    return {type}
-}
 
